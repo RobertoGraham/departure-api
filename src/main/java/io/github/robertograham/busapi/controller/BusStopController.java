53d2cc7d9bcf8df125bdc0d59fea85d920c11635
Roberto Graham <robertogrhm@gmail.com>
2019-07-17 11:58:00 +0100

    Handle FeignExceptions differently for different endpoints. Upgraded to JUnit 5

--- src/main/java/io/github/robertograham/busapi/controller/BusStopController.java
+++ src/main/java/io/github/robertograham/busapi/controller/BusStopController.java
@@ -1,11 +1,14 @@
 package io.github.robertograham.busapi.controller;
 
+import feign.FeignException;
 import io.github.robertograham.busapi.dto.BusStop;
 import io.github.robertograham.busapi.dto.BusStopDepartures;
 import io.github.robertograham.busapi.service.BusStopService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.math.BigDecimal;
 import java.util.List;
@@ -24,11 +27,26 @@ final class BusStopController {
     @GetMapping(value = "", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     private List<BusStop> getNearbyBusStops(@RequestParam final BigDecimal latitude,
                                             @RequestParam final BigDecimal longitude) {
-        return busStopService.getNearbyBusStops(longitude, latitude);
+        try {
+            return busStopService.getNearbyBusStops(longitude, latitude);
+        } catch (final FeignException feignException) {
+            throw createBadGatewayResponseStatusException();
+        }
     }
 
     @GetMapping(value = "/{busStopId}/departures", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     private BusStopDepartures getBusStopDepartures(@PathVariable final String busStopId) {
-        return busStopService.getBusStopDepartures(busStopId);
+        try {
+            return busStopService.getBusStopDepartures(busStopId);
+        } catch (final FeignException feignException) {
+            if (HttpStatus.NOT_FOUND.value() == feignException.status())
+                throw new ResponseStatusException(HttpStatus.NOT_FOUND, String.format("No bus stop found for id: %s", busStopId), feignException);
+            else
+                throw createBadGatewayResponseStatusException();
+        }
+    }
+
+    private ResponseStatusException createBadGatewayResponseStatusException() {
+        return new ResponseStatusException(HttpStatus.BAD_GATEWAY, "Communication error occurred with data provider");
     }
 }
--- src/test/java/io/github/robertograham/busapi/BusApiApplicationTests.java
+++ src/test/java/io/github/robertograham/busapi/BusApiApplicationTests.java
@@ -1,16 +1,12 @@
 package io.github.robertograham.busapi;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
-public class BusApiApplicationTests {
+class BusApiApplicationTests {
 
     @Test
-    public void contextLoads() {
+    void contextLoads() {
     }
-
 }
