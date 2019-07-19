fcd0c8f006ebed5444ad82a0ab1b61431ed0062b
Roberto Graham <robertogrhm@gmail.com>
2019-07-19 19:26:33 +0100

    Remove unused factory method

--- src/main/java/io/github/robertograham/busapi/factory/BusStopFactory.java
+++ src/main/java/io/github/robertograham/busapi/factory/BusStopFactory.java
@@ -1,11 +1,9 @@
 package io.github.robertograham.busapi.factory;
 
-import io.github.robertograham.busapi.client.dto.BusStopDeparturesResponse;
 import io.github.robertograham.busapi.client.dto.PlacesResponse;
 import io.github.robertograham.busapi.client.dto.Type;
 import io.github.robertograham.busapi.dto.BusStop;
 import org.springframework.stereotype.Component;
-import org.springframework.util.StringUtils;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -15,24 +13,6 @@ public final class BusStopFactory {
 
     public List<BusStop> createBusStopList(final PlacesResponse placesResponse) {
         return createBusStopList(placesResponse.getMember());
-    }
-
-    public BusStop createBusStop(final BusStopDeparturesResponse busStopDeparturesResponse) {
-        final var nameStringBuilder = new StringBuilder(busStopDeparturesResponse.getStopName());
-        if (!StringUtils.isEmpty(busStopDeparturesResponse.getBearing()))
-            nameStringBuilder.append(" - ")
-                    .append(busStopDeparturesResponse.getBearing())
-                    .append("-bound");
-        final var busStopBuilder = BusStop.newBuilder()
-                .id(busStopDeparturesResponse.getAtcoCode())
-                .name(nameStringBuilder.toString())
-                .locality(busStopDeparturesResponse.getLocality());
-        final var coordinatesBigDecimalList = busStopDeparturesResponse.getLocation().getCoordinates();
-        if (coordinatesBigDecimalList.size() == 2)
-            return busStopBuilder.longitude(coordinatesBigDecimalList.get(0))
-                    .latitude(coordinatesBigDecimalList.get(1))
-                    .build();
-        return busStopBuilder.build();
     }
 
     private List<BusStop> createBusStopList(final List<PlacesResponse.Member> members) {
