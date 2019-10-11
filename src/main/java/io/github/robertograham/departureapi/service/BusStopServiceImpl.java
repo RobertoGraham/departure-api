1eca4fe8f9c152f9cac2f48b2955a9a647a8a101
Roberto Graham <robertogrhm@gmail.com>
2019-10-11 23:08:55 +0100

    Prevent illegal reflective access

--- src/main/java/io/github/robertograham/departureapi/service/BusStopServiceImpl.java
+++ src/main/java/io/github/robertograham/departureapi/service/BusStopServiceImpl.java
@@ -3,6 +3,7 @@ package io.github.robertograham.departureapi.service;
 import io.github.robertograham.departureapi.client.TransportApiClient;
 import io.github.robertograham.departureapi.client.dto.Group;
 import io.github.robertograham.departureapi.client.dto.NextBuses;
+import io.github.robertograham.departureapi.client.dto.TypeSet;
 import io.github.robertograham.departureapi.dto.BusStop;
 import io.github.robertograham.departureapi.dto.Departure;
 import io.github.robertograham.departureapi.util.BusStopHelper;
@@ -13,7 +14,11 @@ import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
+import java.util.stream.Collectors;
+
+import static io.github.robertograham.departureapi.client.dto.Type.BUS_STOP;
 
 @Service
 @RequiredArgsConstructor
@@ -24,12 +29,25 @@ final class BusStopServiceImpl implements BusStopService {
 
     @Override
     public List<BusStop> getNearbyBusStops(final BigDecimal longitude, final BigDecimal latitude) {
-        return BusStopHelper.createBusStopList(transportApiClient.nearbyBusStopPlacesMembers(latitude, longitude));
+        return BusStopHelper.createBusStopList(transportApiClient.places(latitude, longitude, null, null, null, null, null, TypeSet.newBuilder()
+            .type(BUS_STOP)
+            .build())
+            .getMembers().stream()
+            .filter(Objects::nonNull)
+            .filter((final var member) -> BUS_STOP == member.getType())
+            .collect(Collectors.toList()));
     }
 
     @Override
     public Optional<BusStop> getBusStop(final String busStopId) {
-        return transportApiClient.busStopPlacesMemberById(busStopId)
+        return transportApiClient.places(null, null, null, null, null, null, busStopId, TypeSet.newBuilder()
+            .type(BUS_STOP)
+            .build())
+            .getMembers().stream()
+            .filter(Objects::nonNull)
+            .filter((final var member) -> BUS_STOP == member.getType())
+            .filter((final var member) -> busStopId.equals(member.getAtcoCode()))
+            .findFirst()
             .map(BusStopHelper::createBusStop);
     }
 
