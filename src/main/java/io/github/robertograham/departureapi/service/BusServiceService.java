c05a3a3ba0a16d97fba839da09c418da1c1080ee
Roberto Graham <robertogrhm@gmail.com>
2019-10-21 17:41:33 +0100

    Removed bus service endpoint

--- src/main/java/io/github/robertograham/departureapi/service/BusServiceService.java
+++
@@ -1,8 +0,0 @@
-package io.github.robertograham.departureapi.service;
-
-import io.github.robertograham.departureapi.client.dto.BusServiceResponse;
-
-public interface BusServiceService {
-
-    BusServiceResponse getBusService(String operatorCode, String line);
-}
