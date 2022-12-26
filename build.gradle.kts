581cdc0cb887f7a350b84644a8429926a8b6dfbc
Roberto Graham <RobertoGraham@users.noreply.github.com>
2022-12-26 21:39:27 +0000

    Gradle (#131)
    * Bump maven from 3.8.3-openjdk-17-slim to 3.8.4-openjdk-17-slim
    
    Bumps maven from 3.8.3-openjdk-17-slim to 3.8.4-openjdk-17-slim.
    
    ---
    updated-dependencies:
    - dependency-name: maven
      dependency-type: direct:production
      update-type: version-update:semver-patch
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Bump azul/zulu-openjdk-alpine
    
    Bumps azul/zulu-openjdk-alpine from 17.0.0-17.28.13-jre-headless to 17.0.1-jre-headless.
    
    ---
    updated-dependencies:
    - dependency-name: azul/zulu-openjdk-alpine
      dependency-type: direct:production
      update-type: version-update:semver-patch
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Bump gmavenplus-plugin from 1.13.0 to 1.13.1
    
    Bumps [gmavenplus-plugin](https://github.com/groovy/GMavenPlus) from 1.13.0 to 1.13.1.
    - [Release notes](https://github.com/groovy/GMavenPlus/releases)
    - [Commits](https://github.com/groovy/GMavenPlus/compare/1.13.0...1.13.1)
    
    ---
    updated-dependencies:
    - dependency-name: org.codehaus.gmavenplus:gmavenplus-plugin
      dependency-type: direct:production
      update-type: version-update:semver-patch
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * chore(deps): Upgrades
    
    * chore(deps): Upgrades
    
    * chore(deps): Upgrades
    
    * chore(deps): Upgrades
    
    * chore(deps): Upgrades
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

---
+++ build.gradle.kts
@@ -0,0 +1,38 @@
+plugins {
+    java
+    groovy
+    id("org.springframework.boot") version "3.0.1"
+    id("io.spring.dependency-management") version "1.1.0"
+}
+
+group = "io.github.robertograham"
+version = "0.0.1-SNAPSHOT"
+description = "Departure API"
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation(platform("org.springframework.cloud:spring-cloud-dependencies:2022.0.0"))
+    implementation("org.springframework.cloud:spring-cloud-starter-openfeign")
+    implementation("org.apache.commons:commons-lang3")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation(platform("org.spockframework:spock-bom:2.3-groovy-4.0"))
+    testImplementation("org.spockframework:spock-core")
+    testImplementation("org.spockframework:spock-spring")
+}
+
+tasks.test {
+    useJUnitPlatform()
+    testLogging {
+        events("passed", "skipped", "failed")
+    }
+}
--- src/test/groovy/io/github/robertograham/departureapi/controller/BusStopControllerTests.groovy
+++ src/test/groovy/io/github/robertograham/departureapi/controller/BusStopControllerTests.groovy
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.context.annotation.Bean
+import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.web.servlet.MockMvc
 import spock.lang.Specification
 import spock.mock.DetachedMockFactory
@@ -25,6 +26,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
 @WebMvcTest(BusStopController)
+@ContextConfiguration
 final class BusStopControllerTests extends Specification {
 
     @Autowired
